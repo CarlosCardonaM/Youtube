6b9cb0a6af97df5e192e1c6f5f56c4748acc3b15
Carlos Cardona <cardonacarlos97.06@gmail.com>
2020-06-29 20:11:22 -0500

    Since lesson number 5

---
+++ Youtube/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Youtube
+//
+//  Created by Carlos Cardona on 29/06/20.
+//  Copyright © 2020 D O G. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = "UU4LHNX8d8RqnDX0OezgmCTg"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
